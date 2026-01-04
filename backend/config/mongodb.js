d1429bab3070f0c92ed1696e1dad46f1722d3090
Lucky <luckychaudhary6417@gmail.com>
2026-01-04 23:33:34 +0530

    fix mongo env variable name

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+   await mongoose.connect(`${process.env.MONGODB_URI}/appointy`);
     console.log("Database Connected");
   } catch (error) {
     console.error("Database connection error:", error);
