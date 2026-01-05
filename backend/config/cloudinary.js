25f663973c6fafe988ad34a951d71213de774328
Lucky <luckychaudhary6417@gmail.com>
2026-01-05 16:39:48 +0530

    add

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,19 +1,19 @@
-import { v2 as cloudinary } from 'cloudinary';
+// import { v2 as cloudinary } from 'cloudinary';
 
-const connectCloudinary = async () => {
+// const connectCloudinary = async () => {
 
-    cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRET
-    });
+//     cloudinary.config({
+//         cloud_name: process.env.CLOUDINARY_NAME,
+//         api_key: process.env.CLOUDINARY_API_KEY,
+//         api_secret: process.env.CLOUDINARY_API_SECRET
+//     });
 
-}
-console.log(
-  "CLOUDINARY CHECK:",
-  process.env.CLOUDINARY_NAME,
-  process.env.CLOUDINARY_API_KEY ? "KEY_OK" : "KEY_MISSING",
-  process.env.CLOUDINARY_API_SECRET ? "SECRET_OK" : "SECRET_MISSING"
-);
+// }
+// console.log(
+//   "CLOUDINARY CHECK:",
+//   process.env.CLOUDINARY_NAME,
+//   process.env.CLOUDINARY_API_KEY ? "KEY_OK" : "KEY_MISSING",
+//   process.env.CLOUDINARY_API_SECRET ? "SECRET_OK" : "SECRET_MISSING"
+// );
 
-export default connectCloudinary;
+// export default connectCloudinary;
