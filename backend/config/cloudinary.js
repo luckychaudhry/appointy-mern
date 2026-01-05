93cc557f4cf2aaaf1cb3feed6dbdd1b5a785a778
Lucky <luckychaudhary6417@gmail.com>
2026-01-05 15:05:10 +0530

    force cloudinary config debug

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,13 +1,15 @@
 import { v2 as cloudinary } from "cloudinary";
 
 const connectCloudinary = () => {
+  console.log("🔥 CLOUDINARY FILE LOADED");
+
   cloudinary.config({
     cloud_name: "dswngnvnl",
     api_key: "529346893948471",
     api_secret: "W3xO9U13xTBPfZJKR3kInp84_-I",
   });
 
-  console.log("Cloudinary connected");
+  console.log("🔥 CLOUDINARY CONFIG DONE");
 };
 
 export default connectCloudinary;
