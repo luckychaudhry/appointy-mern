24fb2a1f1da78ced2fb0144ee2ece46af0c16815
Lucky <luckychaudhary6417@gmail.com>
2026-01-05 14:55:12 +0530

    temporary hardcode cloudinary config

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,20 +1,13 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 
-const connectCloudinary = async () => {
-
+const connectCloudinary = () => {
   cloudinary.config({
-  cloud_name: "dswngnvnl",
-  api_key: "529346893948471",
-  api_secret: "W3xO9U13xTBPfZJKR3kInp84_-I",
-});
+    cloud_name: "dswngnvnl",
+    api_key: "529346893948471",
+    api_secret: "W3xO9U13xTBPfZJKR3kInp84_-I",
+  });
 
-
-}
-console.log(
-  "CLOUDINARY CHECK:",
-  process.env.CLOUDINARY_NAME,
-  process.env.CLOUDINARY_API_KEY ? "KEY_OK" : "KEY_MISSING",
-  process.env.CLOUDINARY_API_SECRET ? "SECRET_OK" : "SECRET_MISSING"
-);
+  console.log("Cloudinary connected");
+};
 
 export default connectCloudinary;
